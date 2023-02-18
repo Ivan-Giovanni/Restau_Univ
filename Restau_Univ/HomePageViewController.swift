4435c9886f988f03c916c93bf5e414fd7f37e3d9
Ivan-Giovanni <zanguegiovanni@gmail.com>
2023-02-18 08:30:06 +0100

    HomePage complete

--- Restau_Univ/HomePageViewController.swift
+++ Restau_Univ/HomePageViewController.swift
@@ -16,17 +16,9 @@ class HomePageViewController: UITableViewController {
         
         //MARK: Attaching Images to the CollectionView
         
-        models.append(Model(text: "First", imageName: "Image1"))
-        models.append(Model(text: "Second", imageName: "Image2"))
+        models.append(Model(text: "Mood", imageName: "Image1"))
+        models.append(Model(text: "Elegant", imageName: "Image2"))
         models.append(Model(text: "Third", imageName: "Image3"))
-        models.append(Model(text: "Light", imageName: "Image4"))
-        models.append(Model(text: "Mood", imageName: "Image5"))
-        models.append(Model(text: "Moddy", imageName: "Image6"))
-        models.append(Model(text: "Elegant", imageName: "Image7"))
-        models.append(Model(text: "Smart", imageName: "Image8"))
-        models.append(Model(text: "Tiny", imageName: "Image9"))
-        models.append(Model(text: "Soft", imageName: "Image10"))
-        models.append(Model(text: "Swift", imageName: "Image11"))
         models.append(Model(text: "Rock", imageName: "Image12"))
 
         tableView.register(CollectionTableViewCell.nib(), forCellReuseIdentifier: CollectionTableViewCell.identifier)
@@ -39,7 +31,7 @@ class HomePageViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CollectionTableViewCell.identifier, for: indexPath) as! CollectionTableViewCell
         cell.configure(with: models)
-        
+
         return cell
     }
     
